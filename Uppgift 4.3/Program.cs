b828064f6f45c69ab864ec1b48ef0e30ee6a2bbb
Admin <Admin@TEB123013>
2023-11-17 15:25:56 +0100

    Add project files.

---
+++ Uppgift 4.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Meddelande";
+            
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+    }
+}
